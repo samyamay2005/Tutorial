6d7da1ce0f4532e9e6ce2a2b4e218b99906f71bd
Samyamay Bhattacharjee <samyamay2005@gmail.com>
2025-09-09 02:18:35 +0530

    JS added

---
+++ script.js
@@ -0,0 +1,25 @@
+let n = 10.4;
+console.log(n);
+var v= 4015;
+console.log(v);
+let string = 'Samya is learning js';
+console.log(string);
+let sym= Symbol('unique');
+
+console.log(sym);
+
+let m=108;
+let u=108;
+
+console.log(m===u);
+console.log(m==u);
+console.log(m=n);
+
+console.log(m===u);
+
+let dynamicarray=[];
+dynamicarray.push["sam"];
+dynamicarray.push["Samya"];
+dynamicarray.push["samyamay"];
+
+console.log(dynamicarray);
